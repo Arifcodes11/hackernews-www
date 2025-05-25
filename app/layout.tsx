d24286216bbb1aa35eebdeb34a1af16eda8cb36f
Arifcodes11 <arifcodes11@gmail.com>
2025-05-26 01:50:57 +0530

    Refactor RootLayout: clean up code and improve session handling by removing unnecessary checks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-
-
 import { PropsWithChildren } from "react";
 import "./globals.css";
 import { Inter } from "next/font/google";
@@ -8,28 +6,31 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { TanstackQueryClientProvider } from "@/components/providers/tanstack-query-client-provider";
 import { Toaster } from "@/components/ui/sonner";
 import NavigationBar from "@/components/ui/NavigationBar";
-import { betterAuthClient } from "@/lib/integrations/better-auth";
 
-const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Intelliview",
-  description: "Breaking barriers in news—tech, trends, and world affairs delivered with depth and clarity.",
+  description:
+    "Breaking barriers in news—tech, trends, and world affairs delivered with depth and clarity.",
 };
 
 const RootLayout = ({ children }: PropsWithChildren) => {
-  const { data, isPending } = betterAuthClient.useSession();
-
-  if (isPending) return null;
-  const isLoggedIn = Boolean(data?.user);
-
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <TanstackQueryClientProvider>
             <main className="min-h-screen">
-              {isLoggedIn && <NavigationBar />}
+              <NavigationBar />
               <div className="mx-auto mt-4 px-4 sm:px-6 lg:px-8 max-w-7xl">
                 {children}
               </div>
