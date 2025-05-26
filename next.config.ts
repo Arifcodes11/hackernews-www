2009b9cccb09cf0e24c7f444b941ab4dc878b5ea
Arifcodes11 <arifcodes11@gmail.com>
2025-05-27 02:47:06 +0530

    Refactor README.md: update project title and remove unnecessary sections; update middleware matcher formatting; update package-lock.json dependencies to latest versions

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/feeds/:path*",
-        destination: "http://localhost:3000/feeds/:path*", // 👈 backend on port 3000
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
